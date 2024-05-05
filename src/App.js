25455c913526d05469cc8ae74f314c31ac3c295d
dinogarlic <xldpsl1003@gmail.com>
2024-05-06 04:14:22 +0900

    Feat: 서버 연동 없이 화면 구현까지 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import Main from "./pages/Main";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <h1>ELSA</h1>
-    </div>
+    <StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/Admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </StyledApp>
   );
 }
 
 export default App;
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justifycontent: center;
+  align-items: center;
+`;
---
+++ src/components/ProjectTitle.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ProjectTitle = () => {
+  return (
+    <StyledProjectTitle>
+      Artificial Intelligence Ethics Evaluation System
+    </StyledProjectTitle>
+  );
+};
+
+export default ProjectTitle;
+
+const StyledProjectTitle = styled.h1`
+  margin: 4rem 0 2.5rem 0;
+`;
