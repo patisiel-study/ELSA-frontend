88c5fae63d8729bad634c42e555cb45b96f0958b
Dinogarlic <xldpsl1003@gmail.com>
2024-10-13 20:57:31 +0900

    Feat: 검사 페이지 생성

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -4,7 +4,7 @@ import Menu from "../components/Menu";
 import { Header, Title, Content } from "../components/Header";
 import BlueCurve from "../components/BlueCurve";
 import Footer from "../components/Footer";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { color } from "../color";
 
 const Test = () => {
@@ -12,11 +12,20 @@ const Test = () => {
     <HomepageLayout>
       <Menu />
       <Header>
-        <Title>AI 윤리 검사</Title>
-        <Content>AI 윤리 검사 설명~~~</Content>
+        <Title>인공지능 윤리 검사</Title>
+        <Content>인공지능 윤리 검사 설명~~~</Content>
       </Header>
       <BlueCurve />
-      <TestMainContent></TestMainContent>
+      <TestMainContent>
+        <ButtonContainer>
+          <AIorUserButton>
+            <ButtonText>개발 윤리 검사</ButtonText>
+          </AIorUserButton>
+          <AIorUserButton>
+            <ButtonText>사용자 윤리 검사</ButtonText>
+          </AIorUserButton>
+        </ButtonContainer>
+      </TestMainContent>
       <Footer />
     </HomepageLayout>
   );
@@ -24,10 +33,61 @@ const Test = () => {
 
 export default Test;
 
+const buttonHover = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.05);
+  }
+`;
+
+const buttonShrink = keyframes`
+  0% {
+    transform: scale(1.05);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
 const TestMainContent = styled.main`
-  height: 50rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
   background-color: ${color.primary};
 `;
 
-const AIorUserButton = styled.button``;
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const ButtonText = styled.p`
+  color: ${color.primary};
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
+
+const AIorUserButton = styled.button`
+  height: 15rem;
+  width: 20rem;
+  margin: 2rem 3rem;
+  background-color: white;
+  border: none;
+  border-radius: 3rem;
+
+  &:hover {
+    background-color: ${color.accent};
+    cursor: pointer;
+    animation: ${buttonHover} 0.3s forwards;
+
+    ${ButtonText} {
+      color: white;
+    }
+  }
+
+  &:not(:hover) {
+    animation: ${buttonShrink} 0.3s forwards; /* hover에서 벗어나면 축소 애니메이션 */
+  }
+`;
